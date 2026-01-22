193968d182aef996a4c04e6f1a5eddf8b0d25b15
Mananas <fran@>
2026-01-22 13:54:52 +0100

    66

--- src/com/barcos/Main.java
+++ src/com/barcos/Main.java
@@ -1,7 +1,8 @@
 package com.barcos;
-
+import com.barcos.game.Board;
 public class Main {
     public static void main(String[] args) {
-
+Board board = new Board(10);
+        System.out.println(board.render(true));
     }
 }
--- src/com/barcos/game/Board.java
+++ src/com/barcos/game/Board.java
@@ -1,42 +1,50 @@
 package com.barcos.game;
 
 public class Board {
-private final int size;
-private final CellState[][] grid;
-
+    private final int size;
+    private final CellState[][] grid;
 
     public Board(int size) {
         this.size = size;
         this.grid = new CellState[size][size];
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
 
-        for(int r = 0; r<size ;r++){
-            for(int c = 0; c<size ;c++){
-
-                grid[r][c]= CellState.EMPTY;
+                grid[r][c] = CellState.EMPTY;
             }
-
-
         }
-
-
-
     }
 
-    public String render(){
+    public String render(boolean showShips) {
         StringBuilder sb = new StringBuilder();
-
-
-        for(int r = 0; r<size ;r++){
-            for(int c = 0; c<size ;c++){
-
-                if (grid[r][c] == CellState.EMPTY){
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
+                if (grid[r][c] == CellState.EMPTY) {
                     sb.append(". ");
-                    sb.append("\n");
-
+                } else if (grid[r][c] == CellState.MISS) {
+                    sb.append("~ ");
+                }else if (grid[r][c] == CellState.HIT) {
+                    sb.append("X ");
+                }else if (grid[r][c] == CellState.SHIP) {
+                    if (showShips) {
+                        sb.append("S ");
+                    } else {
+                        sb.append(". ");
+                    }
                 }
             }
-
+            sb.append("\n");
         }
+
+
+
+
+
+
+
+
+
+
         return sb.toString();
     }
 }
