643b021510a5fc2dfef5411e362a6c174798c91b
Fran <d@>
2026-01-26 08:14:37 +0100

    wa

--- src/com/barcos/game/Board.java
+++ src/com/barcos/game/Board.java
@@ -24,9 +24,52 @@ public class Board {
 
     public boolean placeShipCell(int row, int col){
 
-        if (isInside(row, col)) &&
+        if (!isInside(row, col)) return false;
+        if (grid [row][col] != CellState.EMPTY) return false;
+        grid [row][col] = CellState.SHIP;
+        return true;
 
+    }
 
+    public boolean placeShip(int row, int col, int length, boolean horizontal){
+// 1) Comprobar que el barco cabe (no se sale del tablero)
+        if (horizontal) {
+            if (col + length > size) return false;
+        } else {
+            if (row + length > size) return false;
+        }
+
+        // 2) Comprobar que todas las casillas están vacías (no pisa otro barco)
+        for (int l = 0;l < length; l++ ){
+            int r = horizontal ? row : row + l;
+            int c = horizontal ? col + l : col;
+            if (grid[r][c] != CellState.EMPTY)
+            {return false;}
+
+        }
+        // 3) Colocar el barco (marcar casillas como SHIP)
+        for (int j = 0;j < length; j++ ){
+            int r = horizontal ? row : row + j;
+            int c = horizontal ? col + j : col;
+            grid[r][c] = CellState.SHIP;
+        }
+        return true;
+
+    }
+
+    public boolean shoot(int row, int col){
+        // 1) Dentro del tablero
+        if (!isInside(row, col)) return false;
+
+        // 2) No disparar dos veces
+        if (grid [row][col] == CellState.HIT || grid [row][col] == CellState.MISS) return false;
+
+        // 3) Aplicar disparo
+        if (grid [row][col] == CellState.SHIP){
+            grid [row][col] = CellState.HIT;
+            return true;
+        }  grid[row][col] = CellState.MISS;
+        return false;
 
     }
 
@@ -43,20 +86,19 @@ public class Board {
 
     public String render(boolean showShips) {
         StringBuilder sb = new StringBuilder();
+
         for (int r = 0; r < size; r++) {
             for (int c = 0; c < size; c++) {
-                if (grid[r][c] == CellState.EMPTY) {
+                CellState cell = grid[r][c];
+
+                if (cell == CellState.EMPTY) {
                     sb.append(". ");
-                } else if (grid[r][c] == CellState.MISS) {
+                } else if (cell == CellState.MISS) {
                     sb.append("~ ");
-                }else if (grid[r][c] == CellState.HIT) {
+                } else if (cell == CellState.HIT) {
                     sb.append("X ");
-                }else if (grid[r][c] == CellState.SHIP) {
-                    if (showShips) {
-                        sb.append("S ");
-                    } else {
-                        sb.append(". ");
-                    }
+                } else if (cell == CellState.SHIP) {
+                    sb.append(showShips ? "S " : ". ");
                 }
             }
             sb.append("\n");
